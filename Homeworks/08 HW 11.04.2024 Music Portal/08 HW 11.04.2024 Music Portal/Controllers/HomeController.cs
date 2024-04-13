052338a554a734fc0aa73cc46b2e7d12b4223fcc
Babchinsky <babchinsky.prog@gmail.com>
2024-04-13 22:10:20 +0300

    Сделал вывод о нике пользователя по сессии

--- Homeworks/08 HW 11.04.2024 Music Portal/08 HW 11.04.2024 Music Portal/Controllers/HomeController.cs
+++ Homeworks/08 HW 11.04.2024 Music Portal/08 HW 11.04.2024 Music Portal/Controllers/HomeController.cs
@@ -1,20 +1,32 @@
-using _08_HW_11._04._2024_Music_Portal.Models;
+﻿using _08_HW_11._04._2024_Music_Portal.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace _08_HW_11._04._2024_Music_Portal.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MusicPortalContext _context;
 
-        public HomeController()
+        public HomeController(MusicPortalContext context)
         {
-            
+            _context = context;
         }
+
 
         public IActionResult Index()
         {
-            return View();
+            var userId = HttpContext.Session.GetInt32("UserId");
+            var user = _context.Users.FirstOrDefault(u => u.Id == userId);
+            //var user = _messagesRepository.GetUserById(Convert.ToInt32(userId));
+
+            // Проверяем, равен ли userName null, и устанавливаем ViewData["Name"] в null, если да
+            ViewData["Name"] = user != null ? user.Login : "Гость";
+
+            var songs = _context.Songs.Include(s => s.Artist).Include(s => s.Genre).ToList();
+
+            return View(songs);
         }
     }
 }
--- Homeworks/08 HW 11.04.2024 Music Portal/08 HW 11.04.2024 Music Portal/Models/MusicPortalContext.cs
+++ Homeworks/08 HW 11.04.2024 Music Portal/08 HW 11.04.2024 Music Portal/Models/MusicPortalContext.cs
@@ -11,5 +11,6 @@ namespace _08_HW_11._04._2024_Music_Portal.Models
         public DbSet<User> Users { get; set; }
         public DbSet<Song> Songs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
     }
 }
