ee97e3425023791bd7e1e6a9920e9c52a636e1d9
Babchinsky <babchinsky.prog@gmail.com>
2024-05-02 17:18:37 +0300

    Сделал пагинацию

--- Homeworks/08 HW 11.04.2024 Music Portal/08 HW 11.04.2024 Music Portal/Controllers/HomeController.cs
+++ Homeworks/08 HW 11.04.2024 Music Portal/08 HW 11.04.2024 Music Portal/Controllers/HomeController.cs
@@ -3,12 +3,13 @@ using _08_HW_11._04._2024_Music_Portal.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Drawing.Printing;
 
 namespace _08_HW_11._04._2024_Music_Portal.Controllers
 {
     public class HomeController : Controller
     {
-        //private readonly MusicPortalContext _context;
+        private readonly MusicPortalContext _context;
         IUsersRepository _usersRepository;
         ISongsRepository _songsRepository;
 
@@ -16,102 +17,59 @@ namespace _08_HW_11._04._2024_Music_Portal.Controllers
         //{
         //    _context = context;
         //}
-        public HomeController(IUsersRepository usersRepository, ISongsRepository songsRepository)
+        public HomeController(MusicPortalContext context, IUsersRepository usersRepository, ISongsRepository songsRepository)
         {
+            _context = context;
             _usersRepository = usersRepository;
             _songsRepository = songsRepository;
         }
 
 
-        public async Task<IActionResult> Index(SortState sortOrder = SortState.NameAsc)
+        public async Task<IActionResult> Index(SortState sortOrder = SortState.NameAsc, int page = 1)
         {
             var userId = HttpContext.Session.GetInt32("UserId");
             var user = await _usersRepository.GetFirstOrDefaultUserAsync(userId ?? 0);
 
             ViewData["Name"] = user?.Login ?? "Гость";
 
-            //var songs = await _context.Songs
-            //                         .Include(s => s.Artist)
-            //                         .Include(s => s.Genre)
-            //                         .ToListAsync();
-            //var songs = await _songsRepository.GetSongsListAsync();
-
-            //var users = await _context.Users.ToListAsync();
             var users = await _usersRepository.GetUsersListAsync();
 
-            //var viewModel = new IndexViewModel
-            //{
-            //    Users = users,
-            //    Songs = songs
-            //};
-
+            
             ViewData["CurrentUser"] = user;
 
-            //return View(viewModel);
+            
 
 
-            //// Подключаем Genre и Artist для Song
-            //IQueryable<Song>? songs = _context.Songs
-            //                                  .Include(x => x.Genre)   // Включаем Genre
-            //                                  .Include(x => x.Artist); // Включаем Artist
-
-            //IQueryable<User>? users = _context.Users;
-
-            //songs = sortOrder switch
-            // {
-            //     SortState.NameDesc => songs.OrderByDescending(s => s.Title),
-            //     SortState.ArtistAsc => songs.OrderBy(s => s.Artist!.Name),
-            //     SortState.ArtistDesc => songs.OrderByDescending(s => s.Artist!.Name),
-            //     SortState.GenreAsc => songs.OrderBy(s => s.Genre!.Name),
-            //     SortState.GenreDesc => songs.OrderByDescending(s => s.Genre!.Name),
-            //     SortState.YearAsc => songs.OrderBy(s => s.Year),
-            //     SortState.YearDesc => songs.OrderByDescending(s => s.Year),
-            //     _ => songs.OrderBy(s => s.Title),
-            // };
+            //var songs = await _songsRepository.GetSongsListAsync(); // Асинхронный вызов
+            IQueryable<Song> songs = _context.Songs.Include(s => s.Artist).Include(s => s.Genre);
 
 
-            var songs = await _songsRepository.GetSongsListAsync(); // Асинхронный вызов
-
-            //// Применяем сортировку в зависимости от sortOrder
-            //songs = sortOrder switch
-            //{
-            //    SortState.NameDesc => songs.OrderByDescending(s => s.Title),
-            //    SortState.ArtistAsc => songs.OrderBy(s => s.Artist?.Name),
-            //    SortState.ArtistDesc => songs.OrderByDescending(s => s.Artist?.Name),
-            //    SortState.GenreAsc => songs.OrderBy(s => s.Genre?.Name),
-            //    SortState.GenreDesc => songs.OrderByDescending(s => s.Genre?.Name),
-            //    SortState.YearAsc => songs.OrderBy(s => s.Year),
-            //    SortState.YearDesc => songs.OrderByDescending(s => s.Year),
-            //    _ => songs.OrderBy(s => s.Title),
-            //};
-
-            var orderedSongs = sortOrder switch
+            songs = sortOrder switch
             {
                 SortState.NameDesc => songs.OrderByDescending(s => s.Title),
-                SortState.ArtistAsc => songs.OrderBy(s => s.Artist?.Name),
-                SortState.ArtistDesc => songs.OrderByDescending(s => s.Artist?.Name),
-                SortState.GenreAsc => songs.OrderBy(s => s.Genre?.Name),
-                SortState.GenreDesc => songs.OrderByDescending(s => s.Genre?.Name),
+                SortState.ArtistAsc => songs.OrderBy(s => s.Artist.Name),
+                SortState.ArtistDesc => songs.OrderByDescending(s => s.Artist.Name),
+                SortState.GenreAsc => songs.OrderBy(s => s.Genre.Name),
+                SortState.GenreDesc => songs.OrderByDescending(s => s.Genre.Name),
                 SortState.YearAsc => songs.OrderBy(s => s.Year),
                 SortState.YearDesc => songs.OrderByDescending(s => s.Year),
                 _ => songs.OrderBy(s => s.Title),
             };
-            var songList = orderedSongs.ToList(); // Преобразуем в List после сортировки
+            //var songList = orderedSongs.ToList(); // Преобразуем в List после сортировки
 
-            //IndexViewModel viewModel = new IndexViewModel
-            //{
-            //    Users = await users.ToListAsync(),
-            //    Songs = await songs.ToListAsync(),
-            //    SortViewModel = new SortViewModel(sortOrder)
-            //};
+            int pageSize = 5;   // количество элементов на странице
+            var count = await songs.CountAsync();
+            var items = await songs.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
 
-            IndexViewModel viewModel = new IndexViewModel
-            {
-                Users = await _usersRepository.GetUsersListAsync(),
-                Songs = songList, // Используем songList после преобразования
-                SortViewModel = new SortViewModel(sortOrder),
-            };
+            PageViewModel pageViewModel = new PageViewModel(count, page, pageSize);
 
+
+            IndexViewModel viewModel = new IndexViewModel(
+                users: await _usersRepository.GetUsersListAsync(),
+                songs: items, // Используйте 'items', которые содержат текущую страницу песен
+                sortViewModel: new SortViewModel(sortOrder),
+                pageViewModel: pageViewModel
+             );
             return View(viewModel);
         }
 
--- Homeworks/08 HW 11.04.2024 Music Portal/08 HW 11.04.2024 Music Portal/Models/IndexViewModel.cs
+++ Homeworks/08 HW 11.04.2024 Music Portal/08 HW 11.04.2024 Music Portal/Models/IndexViewModel.cs
@@ -4,6 +4,17 @@
     {
         public IEnumerable<User> Users { get; set; } = new List<User>();
         public IEnumerable<Song> Songs { get; set; } = new List<Song>();
+        public PageViewModel PageViewModel { get; }
         public SortViewModel SortViewModel { get; set; } = new SortViewModel(SortState.NameAsc);
+
+        public IndexViewModel() { }
+
+        public IndexViewModel(IEnumerable<User> users, IEnumerable<Song> songs, SortViewModel sortViewModel, PageViewModel pageViewModel)
+        {
+            Users = users;
+            Songs = songs;
+            SortViewModel = sortViewModel;
+            PageViewModel = pageViewModel;
+        }
     }
 }
